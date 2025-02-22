1be74552ab2e7c25c8a1d727c4460109932b3954
yairjr-code <eduardojad1102@gmail.com>
2025-02-22 06:49:41 -0500

    Línea por línea Python se ejecuta una línea a la vez, de arriba a abajo.

---
+++ patronlinea.py
@@ -0,0 +1,11 @@
+''' Patrón
+# Línea por línea
+Python se ejecuta una línea a la vez, de arriba a abajo.
+
+Podemos imprimir varios mensajes utilizando varias print()funciones. 
+Por ejemplo, si queremos imprimir dos saludos sencillos:
+'''
+print ("        1")
+print ("      2 3")
+print ("    4 5 6")
+print (" 7 6 9 10")
